f2a5a1e975433fef93ea898a9539194d30bdcb00
MPD <mooing_psycho_duck@hotmail.com>
2021-03-31 22:24:47 -0700

    Most erasable members no longer require virtual function calls

--- SmallContainers/erasable.hpp
+++ SmallContainers/erasable.hpp
@@ -40,10 +40,10 @@ namespace mpd {
     class erasable {
         using buffer_t = std::aligned_storage_t<buffer_size, align_size>;
         struct erasing {
-            virtual erasing* copy_construct(erasing* buffer) const noexcept(noexcept_copy) = 0;
-            virtual erasing* move_construct(erasing* buffer) noexcept(noexcept_move) = 0;
-            virtual void copy_assign(erasing*& buffer) const noexcept(noexcept_copy) = 0;
-            virtual void move_assign(erasing*& buffer) noexcept(noexcept_move) = 0;
+            virtual void copy_construct(erasing* buffer, Interface*& ptr) const noexcept(noexcept_copy) = 0;
+            virtual void move_construct(erasing* buffer, Interface*& ptr) noexcept(noexcept_move) = 0;
+            virtual void copy_assign(erasing* buffer, Interface*& ptr) const noexcept(noexcept_copy) = 0;
+            virtual void move_assign(erasing* buffer, Interface*& ptr) noexcept(noexcept_move) = 0;
             virtual bool swap(erasing* other) = 0;
             virtual Interface* get() = 0;
             virtual const Interface* get() const = 0;
@@ -54,32 +54,38 @@ namespace mpd {
             Impl val;
             template<class...Ts>
             erased(in_place_t, Ts&&...vs) : val(std::forward<Ts>(vs)...) {}
-            erasing* copy_construct(erasing* buffer) const noexcept(noexcept_copy) { assert(buffer); return new(buffer)erased(*this); }
-            erasing* move_construct(erasing* buffer) noexcept(noexcept_move) { assert(buffer); return new(buffer)erased(std::move(*this)); }
-            void copy_assign(erasing*& other) const noexcept(noexcept_copy) {
-                assert(other);
-                erased* othererased = dynamic_cast<erased*>(other);
+            void copy_construct(erasing* buffer, Interface*& ptr) const noexcept(noexcept_copy) { 
+                assert(buffer);
+                assert(!ptr);
+                erased* other=new(buffer)erased(*this); 
+                ptr=&(other->val);
+            }
+            void move_construct(erasing* buffer, Interface*& ptr) noexcept(noexcept_move) { 
+                assert(buffer); 
+                assert(!ptr); 
+                erased* other=new(buffer)erased(std::move(*this)); 
+                ptr=&(other->val); 
+            }
+            void copy_assign(erasing* buffer, Interface*& ptr) const noexcept(noexcept_copy) {
+                assert(buffer);
+                erased* othererased = dynamic_cast<erased*>(buffer);
                 if (othererased) {
                     othererased->val = val;
-                }
-                else {
-                    other->~erasing();
-                    erasing* tempptr = other;
-                    other = nullptr;
-                    other = copy_construct(tempptr);
+                } else {
+                    buffer->~erasing();
+                    ptr = nullptr;
+                    copy_construct(buffer, ptr);
                 }
             }
-            void move_assign(erasing*& other) noexcept(noexcept_move) {
-                assert(other);
-                erased* othererased = dynamic_cast<erased*>(other);
+            void move_assign(erasing* buffer, Interface*& ptr) noexcept(noexcept_move) {
+                assert(buffer);
+                erased* othererased = dynamic_cast<erased*>(buffer);
                 if (othererased) {
                     othererased->val = std::move(val);
-                }
-                else {
-                    other->~erasing();
-                    erasing* tempptr = other;
-                    other = nullptr;
-                    other = move_construct(tempptr);
+                } else {
+                    buffer->~erasing();
+                    ptr = nullptr;
+                    move_construct(buffer, ptr);
                 }
             }
             bool swap(erasing* other) {
@@ -101,12 +107,14 @@ namespace mpd {
                 static_assert_if_then_also<noexcept_copy, std::is_nothrow_copy_constructible_v<Impl>>{};
             }
         };
-        erasing* ptr;
+        Interface* ptr;
         buffer_t rawbuffer;
 #if __cplusplus >= 201704L
         erasing* buffer() { return std::launder(reinterpret_cast<erasing*>(&rawbuffer)); }
+        const erasing* buffer() const { return std::launder(reinterpret_cast<const erasing*>(&rawbuffer)); }
 #else
         erasing* buffer() { return reinterpret_cast<erasing*>(&rawbuffer); }
+        const erasing* buffer() const { return reinterpret_cast<const erasing*>(&rawbuffer); }
 #endif
     public:
         constexpr erasable() noexcept :ptr(nullptr), rawbuffer{} {}
@@ -123,26 +131,28 @@ namespace mpd {
         template<class Impl, class allowed = std::enable_if_t<std::is_base_of_v<Interface, Impl>>>
         explicit erasable(Impl&& rhs) noexcept(std::is_nothrow_constructible_v<Impl, Impl&&>) :ptr(nullptr) { emplace(std::move(rhs)); }
         constexpr erasable& operator=(const erasable& other) noexcept(noexcept_copy) {
-            if (other.ptr && ptr) other.ptr->copy_assign(ptr);
-            else if (other.ptr) ptr = other.ptr->copy_construct(buffer());
+            if (other.ptr && ptr) other.buffer()->copy_assign(buffer(), ptr);
+            else if (other.ptr) other.buffer()->copy_construct(buffer(), ptr);
             else reset();
             return *this;
         }
         constexpr erasable& operator=(erasable&& other) noexcept(noexcept_move) {
-            if (other.ptr && ptr) other.ptr->move_assign(ptr);
-            else if (other.ptr) ptr = other.ptr->move_construct(buffer());
+            if (other.ptr && ptr) other.buffer()->move_assign(buffer(), ptr);
+            else if (other.ptr) other.buffer()->move_construct(buffer(), ptr);
             else reset();
             return *this;
         }
         template<class Impl>
         constexpr std::enable_if_t<std::is_base_of_v<Interface, Impl>, erasable&> operator=(const Impl& rhs) {
-            if (ptr && dynamic_cast<Impl*>(ptr->get())) *dynamic_cast<Impl*>(ptr->get()) = rhs;
+            Impl* self;
+            if (ptr && (self=dynamic_cast<Impl*>(ptr))) *self = rhs;
             else emplace(rhs);
             return *this;
         }
         template<class Impl>
         constexpr std::enable_if_t<std::is_base_of_v<Interface, Impl>, erasable&> operator=(Impl&& rhs) {
-            if (ptr && dynamic_cast<Impl*>(ptr->get())) *dynamic_cast<Impl*>(ptr->get()) = std::move(rhs);
+            Impl* self;
+            if (ptr && (self=dynamic_cast<Impl*>(ptr))) *self = std::move(rhs);
             else emplace(std::move(rhs));
             return *this;
         }
@@ -151,29 +161,37 @@ namespace mpd {
 #endif
         constexpr erasable& operator=(std::nullptr_t) { reset(); return *this; }
         ~erasable() { reset(); }
-        constexpr Interface* operator->() noexcept { return ptr->get(); }
-        constexpr const Interface* operator->() const noexcept { return ptr->get(); }
-        constexpr Interface& operator*() noexcept { assert(ptr); return *(ptr->get()); }
-        constexpr const Interface& operator*() const noexcept { assert(ptr); return *(ptr->get()); }
+        constexpr Interface* operator->() noexcept { return ptr; }
+        constexpr const Interface* operator->() const noexcept { return ptr; }
+        constexpr Interface& operator*() noexcept { assert(ptr); return *ptr; }
+        constexpr const Interface& operator*() const noexcept { assert(ptr); return *ptr; }
         constexpr explicit operator bool() const noexcept { return ptr != nullptr; }
-        constexpr Interface* get() noexcept { return ptr ? ptr->get() : nullptr; }
-        constexpr const Interface* get() const noexcept { return ptr ? ptr->get() : nullptr; }
+        constexpr Interface* get() noexcept { return ptr ? ptr : nullptr; }
+        constexpr const Interface* get() const noexcept { return ptr ? ptr : nullptr; }
         constexpr bool has_value() const noexcept { return ptr != nullptr; }
-        constexpr Interface& value() noexcept { assert(ptr); return *(ptr->get()); }
-        constexpr const Interface& value() const noexcept { assert(ptr); return *(ptr->get()); }
-        constexpr Interface& value_or(Interface& impl) noexcept {return ptr ? *(ptr->get()) : impl;}
-        constexpr const Interface& value_or(const Interface& impl) const noexcept { return ptr ? *(ptr->get()) : impl; }
+        constexpr Interface& value() noexcept { assert(ptr); return *ptr; }
+        constexpr const Interface& value() const noexcept { assert(ptr); return *ptr; }
+        constexpr Interface& value_or(Interface& impl) noexcept {return ptr ? *ptr : impl;}
+        constexpr const Interface& value_or(const Interface& impl) const noexcept { return ptr ? *ptr : impl; }
         constexpr void swap(erasable& other) {
-            if (ptr && other.ptr && ptr->swap(other.ptr)) return;
+            if (ptr && other.ptr && buffer()->swap(other.buffer())) return;
             else if (other.ptr && ptr) std::swap(*this, other);
             else if (other.ptr) { operator=(other); other.reset(); }
             else {other = *this; reset();}
         }
-        constexpr void reset() { if (ptr) { ptr->~erasing(); ptr = nullptr; } }
+        constexpr void reset() { if (ptr) { buffer()->~erasing(); ptr = nullptr; } }
         template<class Impl, class...Ts>
-        constexpr void emplace(type_identity<Impl>, Ts&&...vs) noexcept(std::is_nothrow_constructible_v<Impl, Ts...>) { reset(); ptr = new(buffer())erased<Impl>(in_place, std::forward<Ts>(vs)...); }
+        constexpr void emplace(type_identity<Impl>, Ts&&...vs) noexcept(std::is_nothrow_constructible_v<Impl, Ts...>) { 
+            reset(); 
+            erased<Impl>* o = new(buffer())erased<Impl>(in_place, std::forward<Ts>(vs)...); 
+            ptr = o->get();
+        }
         template<class Impl, class...Ts>
-        constexpr void emplace(Ts&&...vs) noexcept(std::is_nothrow_constructible_v<Impl, Ts...>) { reset(); ptr = new(buffer())erased<Impl>(in_place, std::forward<Ts>(vs)...); }
+        constexpr void emplace(Ts&&...vs) noexcept(std::is_nothrow_constructible_v<Impl, Ts...>) {
+            reset();
+            erased<Impl>* o = new(buffer())erased<Impl>(in_place, std::forward<Ts>(vs)...); 
+            ptr = o->get();
+        }
         template<class Impl>
         constexpr std::enable_if_t<std::is_base_of_v<Interface, Impl>, void> emplace(const Impl& rhs) { emplace(type_identity<Impl>{}, rhs); }
         template<class Impl>
